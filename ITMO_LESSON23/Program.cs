bfd4205b39c531a9c013c7d685392315ff1ceeb1
zoxoxx <spaorto740@gmail.com>
2024-11-24 13:10:57 +0300

    LESSON23

---
+++ ITMO_LESSON23/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число длдя которого необходимо получить факториал:");
+double input = Convert.ToInt32(Console.ReadLine());
+await FactotialAsync(input);
+Console.WriteLine("Main отработал");
+static async Task FactotialAsync (double number)
+{
+    Console.WriteLine("Начали считать факториал.");
+    double result = await Task.Run(() => Calculate(number));
+    Console.WriteLine("Закончили считать факториал.");
+    Console.WriteLine($"Факториал:{result} Числа:{number}");
+    Thread.Sleep(3000); 
+}
+static double Calculate(double number)
+{
+    double factorial = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        factorial *= i;
+        Thread.Sleep(500);
+    }
+    return factorial;
+}
